325c87588d3b73770ab0f47084d96567cc2d7aa6
Deji Obasan <dejiobasan@yahoo.com>
2023-02-19 10:34:26 +0100

    updated app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+});
+
+
+
+app.listen(port, function () {
+    console.log("server started at port 3000.");
+});
