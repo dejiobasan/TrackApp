e7d4e9ad582d96dd52d4956454b4926eeebaa1e0
Deji Obasan <dejiobasan@yahoo.com>
2023-04-06 19:33:32 +0100

    .

--- public/js/index.js
+++ public/js/index.js
@@ -15,21 +15,21 @@
 let map;
 
 async function initMap() {
-  // The location of Uluru
+  // The location of User.
   const position = { lat: 6.8047242, lng: 3.1048864 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+  // The map, centered at Users position.
   map = new Map(document.getElementById("map"), {
     zoom: 12,
     center: position,
     mapId: "DEMO_MAP_ID",
   });
 
-  // The marker, positioned at Uluru
+  // The marker, positioned at Users location 
   const marker = new google.maps.Marker({
     position: position,
     map: map,
